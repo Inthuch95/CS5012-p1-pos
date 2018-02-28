6c8a369a9c9c1cdc4bca3f3df1dbfd8d6111296c
March1703 <it41@st-andrews.ac.uk>
2018-02-28 16:31:48 +0000

    Generate transition probability table

--- src/pos_tag.py
+++ src/pos_tag.py
@@ -7,33 +7,57 @@ import nltk
 from nltk import ngrams
 from nltk.corpus import brown
 from nltk.probability import FreqDist
+import csv
 
 if __name__ == '__main__':
-    brown_tagged_sents = brown.tagged_sents()
+    # initialize variables
+    words = []
+    tags = []
+    train_sents = []
+    test_sents = []
+    bigrams = []
+    tags_dist  = []
+    start = ["<s>"]
+    end = ["</s>"]
+    transition_count = {}
+    transition_prob = {}
+    
+    brown_tagged_sents = brown.tagged_sents(tagset="universal")
     brown_sents = brown.sents()
     
     # 90/10 train/test split
     size = int(len(brown_tagged_sents) * 0.9)
-    size = 50
     train_sents = brown_tagged_sents[:size]
-    test_sents = brown_sents[size:size+5]
-    
-    words = []
-    tags = []
+    test_sents = brown_sents[size:]
+
     # create list of words and tags
     for sent in train_sents:
-        words = words + [w for (w,_) in sent]
-        tags = tags + [t for (_,t) in sent]
-    # set of unique tags
-    tags_freq = FreqDist(tags)
-    # dictionary for tag transition probability
-    pairs_freq_dict = dict((tag,0) for tag in tags_freq)
-    for key in pairs_freq_dict.keys():
-        pairs_freq_dict[key] = dict((tag,0) for tag in tags_freq) 
-    # create tag pairs count
+        words += start + [w for (w,_) in sent] + end
+        tags += start + [t for (_,t) in sent] + end
+        
+    # tags distribution
+    tags_dist = FreqDist(tags)
+    # dictionary for tag transition count table
+    transition_count = dict((tag,0) for tag in tags_dist)
+    for key in transition_count.keys():
+        transition_count[key] = dict((tag,0) for tag in tags_dist) 
     bigrams = list(ngrams(tags, 2))
     for i in bigrams:
         key_row = i[0]
         key_col = i[1]
-        pairs_freq_dict[key_row][key_col] += 1
-    print(pairs_freq_dict)
+        transition_count[key_row][key_col] += 1
+    
+    # dictionary for tag transition probability table
+    transition_prob = dict((tag,0) for tag in tags_dist)
+    for key in transition_prob.keys():
+        transition_prob[key] = dict((tag,0) for tag in tags_dist) 
+    for key_row in transition_prob.keys():
+        for key_col in transition_prob[key_row].keys():
+            transition_prob[key_row][key_col] = 1.0 * transition_count[key_col][key_row] / tags_dist[key_col]
+    # write the table to csv
+    with open("test_output.csv", "wb") as f:
+        w = csv.writer( f )
+        tag_names = transition_prob.values()[0].keys()
+        w.writerow([""] + [key for key in transition_prob.values()[0].keys()])
+        for key in transition_prob.keys():
+            w.writerow([key] + [transition_prob[key][tag_name] for tag_name in tag_names])
